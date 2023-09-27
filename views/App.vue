b00ef6eec4137e3b9923411aeec5dbc78c45d516
MathisG179 <mathisg179@gmail.com>
2023-09-26 21:11:33 -0400

    The beginning

---
+++ views/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <h1>ExpenseMate</h1>
+      <nav>
+        <ul class="nav-links">
+          <li><router-link to="/">Home</router-link></li>
+          <li><router-link to="/dashboard">Dashboard</router-link></li>
+          <li><router-link to="/login">Login</router-link></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+<style scoped>
+.header {
+  background-color: #333;
+  color: white;
+  padding: 15px 0;
+}
+
+.container {
+  margin: 0 30px;
+  max-width: 1400px;
+  padding: 0 15px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.nav-links {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  gap: 15px;
+}
+
+.nav-links li {
+  display: inline;
+}
+
+.nav-links a {
+  text-decoration: none;
+  color: white;
+}
+
+h1 {
+  color: #666;
+}
+
+p {
+  color: #666;
+}
+
+button {
+  padding: 10px 20px;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #0056b3;
+}
+</style>
+<script setup>
+</script>
---
+++ views/Home.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="home-container">
+    <h2>Welcome to ExpenseMate!</h2>
+    <p>Struggling to keep track of your spending? Overwhelmed by numerous bills, receipts, and expenses? Say hello to ExpenseMate—your trusted partner in financial management.</p>
+    <p>Our platform offers you a comprehensive view of all your expenditures, breaking them down into easy-to-understand categories and visual diagrams. We simplify budgeting so that you can focus on making better financial decisions.</p>
+    <p>Unlock the power of smart spending. With ExpenseMate, you gain insight, clarity, and control over your financial world. Don't just view your expenses—understand them.</p>
+    <!-- Add a button to route to the dashboard -->
+    <button @click="goToDashboard()">Try it yourself</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    goToDashboard() {
+      this.$router.push('/dashboard');
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+.nav-links li {
+  display: inline;
+}
+
+.nav-links a {
+  text-decoration: none;
+  color: white;
+}
+
+.home-container {
+  text-align: center;
+  margin-top: 50px;
+  background-color: #f9f9f9;
+  border-radius: 8px;
+  padding: 20px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+h1 {
+  color: #333;
+}
+
+p {
+  color: #666;
+}
+
+button {
+  padding: 10px 20px;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #0056b3;
+}
+</style>
