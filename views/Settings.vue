71735364986120e4b62a4ab196a549f4e81e74b2
AxelLns <axel.loones@gmail.com>
2023-09-29 19:08:45 -0400

    Validation on settings

--- views/Settings.vue
+++ views/Settings.vue
@@ -1,84 +1,120 @@
-<script setup>
-import { ref } from "vue";
-import { useStore } from "vuex";
-
-const store = useStore();
-
-const initialName = ref(store.state.user.name);
-const initialAge = ref(store.state.user.age);
-const initialEmail = ref(store.state.user.email);
-const initialPhone = ref(store.state.user.phone);
-const initialPassword = ref(store.state.user.password);
-
-const editedName = ref(store.state.user.name);
-const editedAge = ref(store.state.user.age);
-const editedEmail = ref(store.state.user.email);
-const editedPhone = ref(store.state.user.phone);
-const editedPassword = ref(store.state.user.password);
-
-const editing = ref(false);
-
-function saveChanges() {
-  initialName.value = editedName.value;
-  initialAge.value = editedAge.value;
-  initialEmail.value = editedEmail.value;
-  initialPhone.value = editedPhone.value;
-  initialPassword.value = editedPassword.value;
-
-  const updatedUser = {
-    name: editedName.value,
-    age: editedAge.value,
-    email: editedEmail.value,
-    phone: editedPhone.value,
-    password: editedPassword.value,
-  };
-  store.commit("UPDATE_USER", updatedUser);
-
-  editing.value = false;
-}
-
-function cancelEditing() {
-  editedName.value = initialName.value;
-  editedAge.value = initialAge.value;
-  editedEmail.value = initialEmail.value;
-  editedPhone.value = initialPhone.value;
-  editedPassword.value = initialPassword.value;
-
-  editing.value = false;
-}
-
-const passwordShown = ref("·".repeat(initialPassword.value.length));
-</script>
-
 <template>
-  <body>
+  <div>
     <h1>Settings</h1>
-    <div class="informations" v-if="!editing">
-      <p>Name : {{ initialName }}</p>
-      <p>Age : {{ initialAge }}</p>
-      <p>Email : {{ initialEmail }}</p>
-      <p>Phone : {{ initialPhone }}</p>
-      <p>Password : {{ passwordShown }}</p>
-      <button @click="editing = true">Edit</button>
-    </div>
-    <div class="informations" v-else>
-      <p>Name : <input type="text" v-model="editedName" required /></p>
-      <p>Age : <input type="number" v-model="editedAge" required /></p>
-      <p>Email : <input type="email" v-model="editedEmail" required /></p>
-      <p>
-        Phone :
-        <input type="number" v-model="editedPhone" required />
-      </p>
-      <p>
-        Password : <input type="password" v-model="editedPassword" required />
-      </p>
-      <div class="buttons">
-        <button @click="saveChanges">Save</button>
-        <button @click="cancelEditing">Cancel</button>
+    <form @submit.prevent="saveChanges">
+      <div class="informations" v-if="!editing">
+        <p>Name: {{ initialName }}</p>
+        <p>Age: {{ initialAge }}</p>
+        <p>Email: {{ initialEmail }}</p>
+        <p>Phone: {{ initialPhone }}</p>
+        <p>Password: {{ passwordShown }}</p>
+        <button @click="editing = true">Edit</button>
       </div>
-    </div>
-  </body>
+      <div class="informations" v-else>
+        <p>Name: <input type="text" v-model="editedName" /></p>
+        <p>Age: <input type="number" v-model="editedAge" /></p>
+        <p>Email: <input type="email" v-model="editedEmail" /></p>
+        <p>Phone: <input type="number" v-model="editedPhone" /></p>
+        <p>Password: <input type="password" v-model="editedPassword" /></p>
+        <div class="buttons">
+          <button type="submit">Save</button>
+          <button type="button" @click="cancelEditing">Cancel</button>
+        </div>
+      </div>
+    </form>
+  </div>
 </template>
+
+<script>
+import { useVuelidate } from "@vuelidate/core";
+import { required, email } from "@vuelidate/validators";
+import { useStore } from "vuex";
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const v$ = useVuelidate();
+    const store = useStore();
+
+    const initialName = ref(store.state.user.name);
+    const initialAge = ref(store.state.user.age);
+    const initialEmail = ref(store.state.user.email);
+    const initialPhone = ref(store.state.user.phone);
+    const initialPassword = ref(store.state.user.password);
+
+    const editedName = ref(store.state.user.name);
+    const editedAge = ref(store.state.user.age);
+    const editedEmail = ref(store.state.user.email);
+    const editedPhone = ref(store.state.user.phone);
+    const editedPassword = ref(store.state.user.password);
+
+    const editing = ref(false);
+    const passwordShown = ref("·".repeat(editedPassword.value.length));
+
+    const validations = {
+      editedName: { required },
+      editedAge: { required },
+      editedEmail: { required, email },
+      editedPhone: { required },
+      editedPassword: { required },
+    };
+
+    const saveChanges = () => {
+      if (v$.$invalid) {
+        // Handle validation errors
+        return;
+      }
+
+      initialName.value = editedName.value;
+      initialAge.value = editedAge.value;
+      initialEmail.value = editedEmail.value;
+      initialPhone.value = editedPhone.value;
+      initialPassword.value = editedPassword.value;
+
+      const updatedUser = {
+        name: editedName.value,
+        age: editedAge.value,
+        email: editedEmail.value,
+        phone: editedPhone.value,
+        password: editedPassword.value,
+      };
+
+      store.commit("UPDATE_USER", updatedUser);
+
+      editing.value = false;
+    };
+
+    const cancelEditing = () => {
+      editedName.value = initialName.value;
+      editedAge.value = initialAge.value;
+      editedEmail.value = initialEmail.value;
+      editedPhone.value = initialPhone.value;
+      editedPassword.value = initialPassword.value;
+
+      editing.value = false;
+    };
+
+    return {
+      v$,
+      initialName,
+      initialAge,
+      initialEmail,
+      initialPhone,
+      initialPassword,
+      editedName,
+      editedAge,
+      editedEmail,
+      editedPhone,
+      editedPassword,
+      editing,
+      passwordShown,
+      validations,
+      saveChanges,
+      cancelEditing,
+    };
+  },
+};
+</script>
 
 <style scoped>
 body {
