f0e6053ab1843d48c7c644465b0fb1fe78a09c54
AxelLns <axel.loones@gmail.com>
2023-10-04 18:03:27 -0400

    router fix on reload

--- router.js
+++ router.js
@@ -1,26 +1,33 @@
 // router/index.js
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from './views/Home.vue';
-import Login from './views/Login.vue';
-import Dashboard from './views/Dashboard.vue';
-import About from './views/About.vue';
-import Settings from './views/Settings.vue';
-import PrivacyPolicy from './privacy/PrivacyPolicy.vue';
-import TermsOfService from './privacy/TermsOfService.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "./views/Home.vue";
+import Login from "./views/Login.vue";
+import Dashboard from "./views/Dashboard.vue";
+import About from "./views/About.vue";
+import Settings from "./views/Settings.vue";
+import PrivacyPolicy from "./privacy/PrivacyPolicy.vue";
+import TermsOfService from "./privacy/TermsOfService.vue";
 
 const routes = [
-    { path: '/', component: Home },
-    { path: '/login', component: Login },
-    { path: '/dashboard', component: Dashboard },
-    { path: '/about', component: About },
-    { path: '/settings', component: Settings },
-    { path: '/policy', component: PrivacyPolicy},
-    { path: '/terms', component: TermsOfService},
+  { path: "/", component: Home },
+  { path: "/login", component: Login },
+  { path: "/dashboard", component: Dashboard },
+  { path: "/about", component: About },
+  { path: "/settings", component: Settings },
+  { path: "/policy", component: PrivacyPolicy },
+  { path: "/terms", component: TermsOfService },
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHistory(),
+  routes,
+  scrollBehavior: (to, from, savedPosition) => {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { top: 0 };
+    }
+  },
 });
 
 export default router;
